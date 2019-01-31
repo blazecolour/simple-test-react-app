b998b9017b2d4a2d20e6607005aa5d6d49666886
blazecolour <936x936@gmail.com>
2019-01-31 19:10:04 +0300

    fix: correct keys, fix state.counter

--- src/components/PictureBox/PictureBox.jsx
+++ src/components/PictureBox/PictureBox.jsx
@@ -9,34 +9,41 @@ export default class PictureBox extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { counter: 1, imgs: [pic] };
+    this.state = { counter: 1, imgs: [{ id: 0, newPic: pic }] };
 
     this.handleAdd = this.handleAdd.bind(this);
     this.handleRemove = this.handleRemove.bind(this);
   }
 
   handleAdd() {
-    const { counter, imgs } = this.state;
-    const newImg = pic;
-    this.setState({ counter: counter + 1, imgs: [newImg, ...imgs] });
+    const newImg = { id: uniqId(), newPic: pic };
+    this.setState(({ counter, imgs }) => ({
+      counter: counter + 1,
+      imgs: [newImg, ...imgs]
+    }));
   }
 
   handleRemove() {
-    const { counter, imgs } = this.state;
+    const { imgs } = this.state;
     const newImgs = imgs.slice(0, -1);
-    if (counter > 1) this.setState({ counter: counter - 1, imgs: newImgs });
+    this.setState(({ counter }) => {
+      if (counter > 1) return { counter: counter - 1, imgs: newImgs };
+    });
+  }
+
+  renderPiture() {
+    const { imgs } = this.state;
+    return imgs.map(img => (
+      <Picture key={img.id} src={img.newPic} alt="dan abramov" />
+    ));
   }
 
   render() {
-    const { counter, imgs } = this.state;
+    const { counter } = this.state;
 
     return (
       <div className="picture-box">
-        <div className="picture-list">
-          {imgs.map(img => (
-            <Picture key={uniqId()} src={img} alt="dan abramov" />
-          ))}
-        </div>
+        <div className="picture-list">{this.renderPiture()}</div>
         <div className="counter">{counter}</div>
         <div className="btn-group">
           <Button onClick={this.handleRemove}> - </Button>
